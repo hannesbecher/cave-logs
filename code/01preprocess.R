e1886f67efb33683dba5477988f0b181b6c230a9
Hannes Becher <hannesbecher@gmx.de>
2022-04-29 22:30:01 +0100

    preprocess

---
+++ code/01preprocess.R
@@ -0,0 +1,29 @@
+# Code to prcess and concatenate individual weather files
+# by Hannes Becher
+
+library(chron)
+setwd("~/git_repos/cave-logs/")
+
+files <- dir("data/", pattern = "csv")
+
+read.wdat <- function(fl){
+  read.table(fl,sep=",",
+             header=T,
+             skip=1,
+             col.names = c("timestamp", "tempC", "hum")
+  )
+}
+
+dat <- read.wdat(paste0("data/", files[1]))
+for(i in files[-1]){
+  dat <- rbind(dat, read.wdat(paste0("data/", i)))
+}
+
+head(dat)
+dat$date <- sapply(dat$timestamp, function(x) strsplit(x, " ")[[1]][[1]])
+dat$time <- sapply(dat$timestamp, function(x) strsplit(x, " ")[[1]][[2]])
+dat$chron <- chron(dat$date,
+                     dat$time,
+                     format=c(dates="y-m-d", times="h:m:s"))
+head(dat)
+save(dat, file = "processed/dat.R")
---
+++ code/02visualise.R
@@ -0,0 +1,31 @@
+# Script to analyses the cave log weather data
+# by Hannes Becher
+
+#install.packages("chron")
+#?chron
+library(chron)
+library(rgl)
+
+load(file = "processed/dat.R")
+head(dat)
+str(dat)
+plot(dat$tempC~dat$chron,
+     ylim=c(0, 70),
+     ylab="Measure",
+     xlab="Time")
+points(dat$hum~dat$chron,
+       col="grey")
+grid()
+legend("center",
+       fill=c("grey", "black"),
+       legend = c("Hum (%)", "Temp (C)"))
+
+plot(dat$tempC,
+     dat$hum,
+#     type="l",
+     xlab="Temp (C)",
+     ylab="Hum (%)")
+grid()
+
+# plot in 3d
+plot3d(dat[,c(2, 3, 6)])

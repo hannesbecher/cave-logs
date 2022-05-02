ac23dbeb27bcfead0f6db2e29e1d9cd377da91ba
Hannes Becher <hannesbecher@gmx.de>
2022-05-02 20:18:21 +0100

    newData

--- julia/01preprocess.jl
+++ julia/01preprocess.jl
@@ -3,6 +3,8 @@ Pkg.activate(".")
 #Pkg.add("CSV")
 #Pkg.add("DataFrames")
 #Pkg.add("Plots")
+#Pkg.add("PyPlot")
+#Pkg.add("PlotlyJS")
 using CSV
 using DataFrames
 using Dates
@@ -20,14 +22,19 @@ for i in datFiles[2:end]
     a = CSV.read("data/" * i, DataFrame, delim=",", header=["dati", "tempC", "hum"], skipto=2, )
     append!(dat, a)
 end
-size(dat)
+size(dat)   
 dat[!,:dati] = DateTime.(dat[!,:dati], "yyyy-mm-dd HH:MM:SS")
-
+unique(dat) |> size
 first(dat, 6)
 scatter(dat[!,:dati], dat[!,:tempC], label="Temp (C)")
 scatter!(dat[!,:dati], dat[!,:hum], label="Hum (%)")
 
 # plot hum and temp
 scatter(dat[!,:tempC], dat[!,:hum],label="")
+xlabel!("Temp (C)")
+ylabel!("Hum (%)")
+
+#plotlyjs()
+scatter(dat[!,:tempC], dat[!,:hum], dat[!, :dati],label="")
 xlabel!("Temp (C)")
 ylabel!("Hum (%)")
